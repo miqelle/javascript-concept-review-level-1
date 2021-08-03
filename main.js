262b705b744503d562072ef28ccfeeb510f5b71f
miqelle <miqellec3@gmail.com>
2021-08-03 03:05:51 -0500

    almost done

---
+++ main.js
@@ -0,0 +1,9 @@
+//exercise 1
+//write a function w/ two integer paramaters that returns all the integers between them
+function num (x1, x2) {
+    let a = 1;
+    let b = 10;
+
+}
+//Iterate through each element in the array and print the element
+for (let i = 0)
